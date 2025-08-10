90a0f5f19b75599831b69902456e642248906a09
maximt23-chat <maxtrubitsyn2@mail.ru>
2025-08-10 18:34:12 +0300

    Отдельно познакомился с кастомными type quards

--- src/index-without-react.ts
+++ src/index-without-react.ts
@@ -1,106 +1,40 @@
-//Сужение типов - это когда мы сначала объединили типы, но затем. в какой-то момент, нам нужно их отделить друг от друга и использовать как независимые типы
+//Type guards - это механизм, который помогает сузить типы, но речь сейчас идет о своих кастомных type guards
 
-//Первый способ для примитивов:
-
-function fn1(arg: number | string | boolean) {
-  if (typeof arg === "number") {
-    return arg; //Тут мы с arg можем работать только как с числом
-  } else if (typeof arg === "string") {
-    return arg; //Тут мы с arg можем работать только как с строкой
-  }
-  return arg; //Тут просто вернется boolean
-}
-
-//Первый способ, проверка на точные значения
-function fn2(arg: number | string | null) {
-  if (arg === null) {
-    console.log("this is null");
-    return console.log(arg);
-  } else {
-    console.log("this is not null");
-    return console.log(arg);
-  }
-}
-fn2(null);
-
-/*
-      Также, typescript умен, если мы будем сравинвать между собой arg: number | string | null и какой-нибудь arg2: number, то в такой if будет попадать только
-      number, так как ts видит, что они равны
-*/
-
-//Третий способ, поиск по конкретным полям
-type User1 = {
-  userName: string;
-};
-type User2 = {
-  firstName: string;
-  lastName: string;
-};
-function fn3(args: User1 | User2) {
-  if ("userName" in args) {
-    return args; //Тут будет только User1 type
-  }
-  if ("firstName" in args) {
-    return args; //Тут будет уже User2 type
-  }
-  return args; //тут будет never, т.к. мы с помощью userName и fristName обработали оба поля
-}
-
-//Четертвый способ, работает только с классами
-class BMW {
-  isBmw() {}
-}
-class Audi {
-  isAudi() {}
-}
-const bwm = new BMW();
-const audi = new Audi();
-
-function f4(args: BMW | Audi) {
-  if (args instanceof BMW) {
-    args.isBmw();
-  }
-  if (args instanceof Audi) {
-    args.isAudi();
-  }
-}
-
-//Пятый способ, очень часто используемый:
-interface BaseCar {
-  numberCar: number;
+interface Car {
   description: string;
+  numberCar: number;
 }
-
-interface Bmw extends BaseCar {
+interface BMW extends Car {
   type: "bmw";
-  bmwString: string;
+  bwnString: string;
 }
-interface Toyota extends BaseCar {
-  type: "toyota";
-  toyotaString: string;
-}
-interface Audi extends BaseCar {
+interface Audi extends Car {
   type: "audi";
   audiString: string;
 }
-interface Lada extends BaseCar {
-  type: "lada";
-  ladaString: string;
+interface Person {
+  name: string;
+  age: number;
 }
-type Car = Bmw | Toyota | Audi | Lada;
-function fn5(args: Car) {
-  switch (args.type) {
-    case "bmw":
-      args.bmwString; //bmw + BaseCar
-      break;
-    case "toyota":
-      args.toyotaString; //toyota + BaseCar
-      break;
-    case "audi": //audi + BaseCar
-      args.audiString;
-      break;
-    default:
-      args; //Lada
+
+function isCar(value: Car | Person): value is Car {
+  return "description" in value && "numberCar" in value;
+}
+function isPerson(value: Car | Person): value is Person {
+  return "name" in value && "age" in value;
+}
+function isBmw(value: BMW | Audi): value is BMW {
+  return value.type === "bmw";
+}
+function isAudi(value: BMW | Audi): value is Audi {
+  return value.type === "audi";
+}
+
+function fn(data: Person | Car) {
+  if (isCar(data)) {
+    return data.numberCar;
   }
-  args; //Car
+  if (isPerson(data)) {
+    return data.name;
+  }
 }
